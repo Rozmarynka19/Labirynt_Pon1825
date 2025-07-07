37c3ee7daf0aff07e9d8c2d0cd17b2b22095597b
Rozmarynka19 <morozmarynowska@gmail.com>
2025-07-07 20:07:46 +0200

    Gotowy portal

--- Assets/Scripts/PortalCamera.cs
+++ Assets/Scripts/PortalCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,7 +11,22 @@ public class PortalCamera : MonoBehaviour
 
     void Update()
     {
-
+        PortalCameraController();
     }
 
+    private void PortalCameraController()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float portalRotDiff = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+        Quaternion portalRotDiffQuat = Quaternion.AngleAxis(portalRotDiff, Vector3.up);
+        Vector3 newCameraDirection = portalRotDiffQuat * playerCamera.forward;
+        newCameraDirection = new Vector3(
+            newCameraDirection.x * -1,
+            newCameraDirection.y,
+            newCameraDirection.z * -1
+            );
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    }
 }
--- Assets/Scripts/PortalTeleport.cs
+++ Assets/Scripts/PortalTeleport.cs
@@ -39,14 +39,18 @@ public class PortalTeleport : MonoBehaviour
         Vector3 portalToPlayer = player.position - transform.position;
         float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
 
-        if (dotProduct > 0)
+        if (dotProduct < 0)
         {
             float rotationDiff =
                 Quaternion.Angle(transform.rotation, receiver.rotation);
             rotationDiff += 180f;
             player.Rotate(Vector3.up, rotationDiff);
 
+            Vector3 positionOffset = 
+                Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+            player.position = receiver.position + positionOffset;
 
+            playerIsOverlapping = false;
         }
     }
 }
